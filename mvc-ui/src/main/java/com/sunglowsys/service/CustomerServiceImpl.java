11f20be5b7685563709c2a2d86345af20192a2bd
DinkiThakur <dinkiraghav@gmail.com>
2022-02-18 11:34:35 +0530

    siley

---
+++ mvc-ui/src/main/java/com/sunglowsys/service/CustomerServiceImpl.java
@@ -0,0 +1,55 @@
+package com.sunglowsys.service;
+import com.sunglowsys.domain.Customer;
+import com.sunglowsys.repository.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+public class CustomerServiceImpl implements CustomerService{
+
+    private final Logger log = LoggerFactory.getLogger(CustomerServiceImpl.class);
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        log.debug("save request:{}",customer);
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public Customer update(Customer customer) {
+        log.debug("update request:{}",customer);
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public Page<Customer> findAll(Pageable pageable) {
+        log.debug("fatch data Request:{}",pageable.toString());
+        return customerRepository.findAll(pageable);
+    }
+
+    @Override
+    public Optional<Customer> findById(Long id) {
+        log.debug("update request:{}",id);
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public void delete(Long id) {
+        log.debug("update request:{}",id);
+        customerRepository.deleteById(id);
+    }
+}
+
